6810e3362ee89005e9c06aab0f24558ee2ee9364
vasanthtcs2009 <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>
2021-05-21 23:12:33 -0700

    completing CountryRepository

--- initial/src/main/java/com/example/producingwebservice/repository/CountryRepository.java
+++ initial/src/main/java/com/example/producingwebservice/repository/CountryRepository.java
@@ -1,6 +1,9 @@
 package com.example.producingwebservice.repository;
 
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.Currency;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -8,10 +11,37 @@ import java.util.Map;
 
 @Component
 public class CountryRepository {
-    //private static final Map<String,Country> countries=new HashMap<>();
+    private static final Map<String, Country> countries = new HashMap<>();
 
     @PostConstruct
-    public void initData(){
+    public void initData() {
+        Country spain = new Country();
+        spain.setName("Spain");
+        spain.setCapital("Madrid");
+        spain.setCurrency(Currency.EUR);
+        spain.setPopulation(908402);
 
+        countries.put(spain.getName(), spain);
+
+        Country poland = new Country();
+        poland.setName("Poland");
+        poland.setCapital("Warsaw");
+        poland.setCurrency(Currency.PLN);
+        poland.setPopulation(38186860);
+
+        countries.put(poland.getName(), poland);
+
+        Country uk = new Country();
+        uk.setName("United Kingdom");
+        uk.setCapital("London");
+        uk.setCurrency(Currency.GBP);
+        uk.setPopulation(63705000);
+
+        countries.put(uk.getName(), uk);
+    }
+
+    public Country findCountry(String name){
+        Assert.notNull(name,"Country name is invalid or null!!");
+        return countries.get(name);
     }
 }
